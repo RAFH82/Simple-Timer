1257544f866b6fcf02de593819e410c7beb756a9
RAFH82 <hrechkar@gmail.com>
2020-11-03 23:44:34 +0000

    refactored code with mentor

--- timer1.js
+++ timer1.js
@@ -1,11 +1,17 @@
 const timerArr = process.argv.slice(2);
-if (timerArr.length > 0) {
-  for (element of timerArr) {
-    if (Number(element) >= 0 && !Number.isNaN(element)) {
-      let delay = Number(element * 1000);
-        setTimeout(() => {
-        process.stdout.write('\x07');
-      }, delay);
-    };
+
+const beep = () => {
+  process.stdout.write("\x07");
+};
+
+const beepTimer = element => {
+  let startCheck = Number(element) >= 0 && !Number.isNaN(element);
+  if (startCheck) {
+    let delay = Number(element * 1000);
+    setTimeout(beep, delay);
   }
 };
+
+for (element of timerArr) {
+  beepTimer(element);
+}
